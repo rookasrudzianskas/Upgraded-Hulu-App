7ea8baa371747b41f6c82eed1f45e9d69e1c00d9
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 08:00:05 +0300

    Rokas

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,7 +13,7 @@ const Nav = ({setSelectedOption}) => {
             <h2 onClick={() => setSelectedOption(requests.fetchHorrorMovies)}>Horror</h2>
             <h2 onClick={() => setSelectedOption(requests.fetchRomanceMovies)}>Romance</h2>
             <h2 onClick={() => setSelectedOption(requests.fetchMystery)}>Mystery</h2>
-            <h2 onClick={() => setSelectedOption(requests.fetchSciFi)}>Sci-Fi</h2>
+            <h2 onClick={() => setSelectedOption(requests.fetchSciFi)}>SciFi</h2>
             <h2 onClick={() => setSelectedOption(requests.fetchWestern)}>Western</h2>
             <h2 onClick={() => setSelectedOption(requests.fetchAnimation)}>Animation</h2>
             <h2 onClick={() => setSelectedOption(requests.fetchTV)}>TV Movie</h2>
