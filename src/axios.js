513ea07a6160c723349b34633950e7b2d9bb8d2f
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 07:02:51 +0300

    Roks

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-// then we make the requests, we add the url tp the fetch urls
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
 });
 
-export default instance
+export default instance;
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import "./styles/Results.css";
 import VideoCard from "./VideoCard";
+import axios from "../axios";
+import requests from "../requests";
 
 const Results = () => {
 
@@ -8,7 +10,21 @@ const Results = () => {
     // the array of the movies
     const [movies, setMovies] = useState([]);
 
+    // this is going to run once and make the api request
+    useEffect(() => {
+        // run code once, then the Results mounts/loads
+        /// async anything what involves going to the server to grab and send the info
 
+        async function fetchData() {
+            // because we wati for the response to come back
+            // it is going to use the base url from the axios instance
+            // it makes the request, with the base url from the axios and the fetch url from the requests
+            const request = await axios.get(requests.fetchActionMovies);
+            console.log("🚀", request);
+
+        }
+        fetchData()
+    }, [])
 
     return (
         <div className="results">
