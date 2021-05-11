2cf31fb11f6a6dc641abd6031c219cdaa77d1a00
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 05:38:53 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import "./styles/Header.css";
+import HomeIcon from "@material-ui/icons/Home";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+import LiveTvIcon from "@material-ui/icons/LiveTv";
+import SearchIcon from "@material-ui/icons/Search";
+import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
 
 const Header = () => {
     return (
         <div className="header">
-            
+            <div className="header__icons">
+            {/*    All icons in here*/}
+            </div>
+
+            <img
+                src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png"
+                alt="Hulu Logo"
+                />
         </div>
     );
 };
