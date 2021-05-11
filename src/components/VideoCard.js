a70e7f44c459dcb2d133fb3baecef1ea593999de
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 07:29:25 +0300

    Rkoas

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import "./styles/VideoCard.css";
-
 // this is image base url
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -8,7 +7,9 @@ const VideoCard  = ({ movie }) => {
     return (
         <div className="videoCard">
             <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt=" movie poster"/>
-            <p>{movie.overview}</p>
+            {/* if the text goes more than one line, and the element is pd, so smaller text with ... and the text is from props movie object*/}
+            {/* in which there is a text*/}
+            <p className="rokas">{movie.overview}</p>
             <h2>{}</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, voluptas?</p>
         </div>
