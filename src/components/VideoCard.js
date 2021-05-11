0bbec42165d5364911999ff7a7a174715eed940f
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 07:11:34 +0300

    Rokas

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -7,8 +7,8 @@ const base_url = "https://image.tmdb.org/t/p/original/";
 const VideoCard  = ({ movie }) => {
     return (
         <div className="videoCard">
-            <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt=""/>
-            <p>Lorem ipsum dolor sit.</p>
+            <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt=" movie poster"/>
+            <p>{movie.overview}</p>
             <h2>{}</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, voluptas?</p>
         </div>
