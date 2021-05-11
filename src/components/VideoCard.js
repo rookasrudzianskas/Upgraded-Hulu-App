a1a7ec597bb290d336e3b72bab096e54cbf7b65f
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 07:31:21 +0300

    Rokas

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import "./styles/VideoCard.css";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+
 // this is image base url
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -10,8 +12,8 @@ const VideoCard  = ({ movie }) => {
             {/* if the text goes more than one line, and the element is pd, so smaller text with ... and the text is from props movie object*/}
             {/* in which there is a text*/}
             <p className="rokas">{movie.overview}</p>
-            <h2>{}</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, voluptas?</p>
+            <h2>{movie.title || movie.original_name}</h2>
+            <p><ThumbUpIcon />{movie.vote_count}</p>
         </div>
     );
 };
